8eb6ec47ecd9764c2ab8faecf262dfeb33823103
Hiroto Yamakawa <1794095+gishi-yama@users.noreply.github.com>
2020-07-06 03:29:49 +0900

    プロメンの練習用にコードを撤去

--- src/main/java/com/example/demo/SampleController.java
+++ src/main/java/com/example/demo/SampleController.java
@@ -1,7 +1,6 @@
 package com.example.demo;
 
 import com.example.demo.java11.Gakusei;
-import com.example.demo.java14.Gakusei14;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,27 +10,39 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class SampleController {
 
+  // デバッグモードの便利さを体験する為だけのfizzコード.
+  // 今回、model引数は使っていないので無視して良い
+  @GetMapping("fizz")
+  @ResponseBody
+  public String getFizz(Model model) {
+    String ans = "";
+    for (int i = 1; i < 20; i++) {
+      if (i % 3 == 0) {
+        ans = ans + "fizz, ";
+      } else {
+        ans = ans + i + ", ";
+      }
+    }
+    return ans;
+  }
+
+  // ブラウザで http://localhost:8080/test にアクセスしたときの処理
+  // 今回、model引数は使っていないので無視して良い
   @GetMapping("test")
   public String getTest(Model model) {
     return "testForm";
   }
 
+  // ブラウザ画面で送信ボタンを押したときの処理
+  // 今回、model引数は使っていないので無視して良い
   @PostMapping("test")
   @ResponseBody
   public String postTest(String gakuseki, String shi, String mei, Model model) {
     try {
-      Gakusei someOne = new Gakusei(gakuseki, shi, mei);
-      return someOne.toPrintableText();
-    } catch (RuntimeException e) {
-      return e.getMessage();
-    }
-  }
-
-  @PostMapping("test14")
-  @ResponseBody
-  public String postTest14(String gakuseki, String shi, String mei, Model model) {
-    try {
-      Gakusei14 someOne = new Gakusei14(gakuseki, shi, mei);
+      // ブラウザ画面から送信された 学籍番号 、氏 、名 を 学生 という構造で管理して（Gakuseki someOne = ...の部分）、
+      // 管理した情報を画面で表示したい（ return someOne.toPrintable. ...の部分）
+      // あなたならクラスをどのように構成し、インスタンス化するか？
+      Gakusei someOne = new Gakusei();
       return someOne.toPrintableText();
     } catch (RuntimeException e) {
       return e.getMessage();
--- src/main/java/com/example/demo/java11/Gakusei.java
+++ src/main/java/com/example/demo/java11/Gakusei.java
@@ -2,16 +2,9 @@ package com.example.demo.java11;
 
 public class Gakusei {
 
-  private final Gakuseki gakuseki;
-  private final FullName fullName;
-
-  public Gakusei(String gakuseki, String shi, String mei) {
-    this.gakuseki = new Gakuseki(gakuseki);
-    this.fullName = new FullName(shi, mei);
-  }
-
   public String toPrintableText() {
-    return String.format("学籍番号は %s 、氏名は %s %s", gakuseki.getValue(), fullName.getShi(), fullName.getMei());
+    // 学籍番号は b1992490 、氏名は 千歳 光 のように返したい。
+    return String.format("学籍番号は %s 、氏名は %s %s", "不明", "不明", "不明");
   }
 }
 
